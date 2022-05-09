577495dcccf12d1f746bf2836ee96a3a566affe6
thediaf <jaharacs98@gmail.com>
2022-05-09 11:13:10 +0000

    feat: student entity

---
+++ src/Student/Entity/Student.java
@@ -0,0 +1,71 @@
+package Student.Entity;
+
+import java.util.Date;
+
+public class Student 
+{
+    private int id;
+    private String lastname;
+    private String firstname;
+    private Date birthDay;
+    private String birthLocate;
+    private String address;
+    private String nationality;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getBirthLocate() {
+        return birthLocate;
+    }
+
+    public void setBirthLocate(String birthLocate) {
+        this.birthLocate = birthLocate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+}
